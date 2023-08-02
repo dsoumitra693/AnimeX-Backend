498a6c5e5d0e166bf23eeae0ceb83a90a315d53d
unknown <dsoumitra693@gmail.com>
2023-08-02 10:59:35 +0530

    debugging the code

--- utils/removeUndefinedVaules.js
+++ utils/removeUndefinedVaules.js
@@ -1,5 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.removeUndefinedVaules = void 0;
 const removeUndefinedVaules = (obj) => {
     Object.keys(obj).forEach(key => obj[key] === undefined && delete obj[key]);
     return obj;
 };
-module.exports = removeUndefinedVaules;
+exports.removeUndefinedVaules = removeUndefinedVaules;
