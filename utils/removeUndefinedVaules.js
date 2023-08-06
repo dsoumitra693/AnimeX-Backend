2deb82bd2744b9c434c680fc7f7a3a417431559d
unknown <dsoumitra693@gmail.com>
2023-08-06 23:16:46 +0530

    solving error

--- utils/removeUndefinedVaules.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.removeUndefinedVaules = void 0;
-const removeUndefinedVaules = (obj) => {
-    Object.keys(obj).forEach(key => obj[key] === undefined && delete obj[key]);
-    return obj;
-};
-exports.removeUndefinedVaules = removeUndefinedVaules;
