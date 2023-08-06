b4dc64a64b7e829257bebd04776d706aab6b2c7d
unknown <dsoumitra693@gmail.com>
2023-08-06 23:19:10 +0530

    solving error

---
+++ public/model/User.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const userSchema = new mongoose_1.Schema({
+    name: { type: String, },
+    email: { type: String },
+    phone: { type: String, resquired: [true, "is require field"] },
+    password: { type: String, resquired: [true, "is require field"] },
+    isSubscribed: { type: Boolean, default: false },
+    favouriteAnime: [{
+            name: String,
+            animeId: String,
+            imgUrl: String,
+        }],
+    watchList: [{
+            name: String,
+            animeId: String,
+            imgUrl: String,
+        }]
+});
+const User = (0, mongoose_1.model)("User", userSchema);
+exports.default = User;
---
+++ public/routes/user.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const user_1 = require("../controllers/user");
+const userRouter = express_1.default.Router();
+userRouter.put('/', user_1.getUserDetails);
+userRouter.put('/update', user_1.updateUserDetails);
+userRouter.put('/fav-anime', user_1.updateFavAnime);
+userRouter.get('/fav-anime', user_1.getFavAnime);
+userRouter.delete('/fav-anime', user_1.deleteFavAnime);
+userRouter.put('/watch-list', user_1.updateWatchList);
+userRouter.get('/watch-list', user_1.getWatchList);
+userRouter.delete('/watch-list', user_1.deleteWatchList);
+exports.default = userRouter;
